04017a7e3bf686337ad4562632e1e913064e4a5e
gespel <heimbrodt@uni-potsdam.de>
2026-05-06 10:05:02 +0200

    refining RQ, adding tooling and adding content for RQ1 aswell as RQ2

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,17 +1,19 @@
 import pandas as pd
 
 class DataLoader:
-    def __init__(self, file_path):
+    def __init__(self):
+        pass
+    
+    def load_overall_production(self, file_path: str):
         try:
             self.data = pd.read_csv(file_path, sep=';', decimal=',')
         except Exception as e:
             print(f"Error loading data: {e}")
-        return None
-    
-    def load_overall_production(self):
+
         if self.data is not None:
             filtered = self.data[self.data["2_variable_attribute_label"] == "Insgesamt"].copy()
             filtered["time"] = pd.to_numeric(filtered["time"], errors='coerce')
             filtered["value"] = pd.to_numeric(filtered["value"], errors='coerce')
             return filtered
         return None
+
---
+++ src/tools.py
@@ -0,0 +1,8 @@
+def date_to_numerical_year(date_str):
+    split = date_str.split("-")
+
+    year = int(split[2])
+    month = int(split[1])
+    day = int(split[0])
+
+    return year + (month - 1) / 12 + (day - 1) / 365
